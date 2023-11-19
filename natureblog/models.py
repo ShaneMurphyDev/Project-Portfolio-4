fcbd36370823353a8f30520c3761b505c00908cc
Shane Murphy <MurphyShaneDev@gmail.com>
2023-11-19 19:37:03 +0000

    Created string rep of the object and like counter

--- natureblog/models.py
+++ natureblog/models.py
@@ -19,3 +19,9 @@ class Post(models.Model):
 
         class Meta:
         ordering = ["-created_on"]
+
+        def __str__(self):
+        return self.title
+
+        def number_of_likes(self):
+        return self.likes.count()
