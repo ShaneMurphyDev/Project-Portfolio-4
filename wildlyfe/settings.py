dfeeb6623449b6693d000fa57c1f97280c03b2fd
Shane Murphy <MurphyShaneDev@gmail.com>
2023-11-20 12:51:40 +0000

    adjusted login page style and templates

--- wildlyfe/settings.py
+++ wildlyfe/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-shanemurphy-projectport-64jj0b791x6.ws-eu106.gitpod.io',
+ALLOWED_HOSTS = ['8000-shanemurphy-projectport-2nl2g3nepi6.ws-eu106.gitpod.io',
 'wildlyfe.herokuapp.com', 'localhost', 'natureblog-2089d93a8eb1.herokuapp.com']
 
 
