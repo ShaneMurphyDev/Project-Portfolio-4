9d2705df1be219cce4c8b5f71e9c91ce2d3ea1df
Shane Murphy <MurphyShaneDev@gmail.com>
2023-11-21 15:06:54 +0000

    created views for sub & sub/success file

--- emailsub/views.py
+++ emailsub/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SubscriptionForm
+from django.core.mail import send_mail
+from django.conf import settings
 
-# Create your views here.
+def subscribe(request):
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            subscription = form.save()
+
+            # Send a confirmation email
+            subject = 'Subscription Confirmation'
+            message = 'Thank you for subscribing!'
+            from_email = settings.DEFAULT_FROM_EMAIL
+            recipient_list = [subscription.email]
+
+            send_mail(subject, message, from_email, recipient_list)
+
+            return render(request, 'subscription/success.html')
+    else:
+        form = SubscriptionForm()
+
+    return render(request, 'subscription/subscribe.html', {'form': form})
