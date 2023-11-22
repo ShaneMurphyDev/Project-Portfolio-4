c1df5fd60b020a3c1bdb22eebcbf07d33fd47cf4
Shane Murphy <MurphyShaneDev@gmail.com>
2023-11-22 12:39:52 +0000

    updated email backend settings

--- wildlyfe/settings.py
+++ wildlyfe/settings.py
@@ -166,8 +166,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'wildlyfeblog@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-
-DEFAULT_FROM_EMAIL = 'wildlyfeblog@gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
