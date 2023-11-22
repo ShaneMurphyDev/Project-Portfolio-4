2c3ad37a76118a10fc77b911cf79846aa3d3fe71
Shane Murphy <MurphyShaneDev@gmail.com>
2023-11-22 13:23:16 +0000

    updated urls

--- natureblog/urls.py
+++ natureblog/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.PostDetail.as_view(), name ='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('add/', views.PostCreate.as_view(), name='post_add'),
+    path('<slug:slug>/edit/', views.PostUpdate.as_view(), name='post_edit'),
+    path('<slug:slug>/delete/', views.PostDelete.as_view(), name='post_delete'),
 ]
