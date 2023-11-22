7ca8de1eaa8742ffb75b92180624e7f821987138
Shane Murphy <MurphyShaneDev@gmail.com>
2023-11-22 12:59:38 +0000

    updated confirmation email

--- emailsub/views.py
+++ emailsub/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import SubscriptionForm
 from django.core.mail import send_mail
 from django.conf import settings
-
+from wildlyfe.settings import EMAIL_HOST_USER
 
 def subscribe(request):
     if request.method == 'POST':
@@ -13,7 +13,7 @@ def subscribe(request):
             # Send a confirmation email
             subject = 'Subscription Confirmation'
             message = 'Thank you for subscribing to the Wildlyfe newsletter!'
-            from_email = settings.DEFAULT_FROM_EMAIL
+            from_email = settings.EMAIL_HOST_USER
             recipient_list = [subscription.email]
 
             send_mail(subject, message, from_email, recipient_list, fail_silently=True)
